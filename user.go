eb427254e4498959809ded5bfc9718245fc149a8
Vladislav Polyakov <polrkov.tm@gmail.com>
2020-09-26 16:46:48 +0300

    fix: remove unused func

--- user.go
+++ user.go
@@ -11,7 +11,3 @@ type User struct {
 	CanReadAllGroupMessages bool   `json:"can_read_all_group_messages,omitempty"`
 	SupportsInlineQueries   bool   `json:"supports_inline_queries,omitempty"`
 }
-
-func NewUser() *User {
-	return &User{}
-}
