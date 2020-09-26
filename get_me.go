4b71d4f4c6a9228b40132b260d4c5629f2f70443
Vladislav Polyakov <polrkov.tm@gmail.com>
2020-09-26 15:03:38 +0300

    doc: add comments

--- get_me.go
+++ get_me.go
@@ -4,11 +4,14 @@ import (
 	"fmt"
 )
 
+// GetMeResponse represents a request response.
+// Returns *User in Result fiend on success.
 type GetMeResponse struct {
 	Response
 	Result *User `json:"result"`
 }
 
+// GetMe returns basic information about the bot in form of a User object.
 func (tg *Telegram) GetMe() (*User, error) {
 	var r GetMeResponse
 
