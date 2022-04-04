070ad8f8cc08150dc21a04025b589a0e2a96e74b
Vladislav Polyakov <polrkov.tm@gmail.com>
2022-04-04 09:38:13 +0300

    refactor: move sticker into specific folder

---
+++ bot/api/mask_position.go
@@ -0,0 +1,29 @@
+package api
+
+type MaskPositionPoint = string
+
+const (
+	MaskPositionPointForeHead MaskPositionPoint = "forehead"
+	MaskPositionPointEyes     MaskPositionPoint = "eyes"
+	MaskPositionPointMouth    MaskPositionPoint = "mouth"
+	MaskPositionPointChin     MaskPositionPoint = "chin"
+)
+
+// MaskPosition describes the position on faces where a mask should be placed by default.
+type MaskPosition struct {
+	// Point is the part of the face relative to which the mask should be placed.
+	// One of MaskPositionPointForeHead, MaskPositionPointEyes, MaskPositionPointMouth, or MaskPositionPointChin.
+	Point MaskPositionPoint `json:"point"`
+
+	// XShift is a shift by X-axis measured in widths of the mask scaled to the face size, from left to right.
+	// For example, choosing -1.0 will place mask just to the left of the default mask position.
+	XShift float64 `json:"x_shift"`
+
+	// YShift is a shift by Y-axis measured in heights of the mask scaled to the face size, from top to bottom.
+	// For example, 1.0 will place the mask just below the default mask position.
+	YShift float64 `json:"y_shift"`
+
+	// Scale is a mask scaling coefficient.
+	// For example, 2.0 means double size.
+	Scale float64 `json:"scale"`
+}
---
+++ bot/api/sticker.go
@@ -0,0 +1,50 @@
+package api
+
+// Sticker is a sticker.
+type Sticker struct {
+	// FileID is an identifier for this file, which can be used to download or reuse the file.
+	FileID string `json:"file_id"`
+
+	// FileUniqueID is a unique identifier for this file,
+	// which is supposed to be the same over time and for different bots.
+	// Can't be used to download or reuse the file.
+	FileUniqueID string `json:"file_unique_id"`
+
+	// Width is a sticker width.
+	Width int `json:"width"`
+
+	// Height is a sticker height.
+	Height int `json:"height"`
+
+	// IsAnimated is True, if the sticker is animated.
+	IsAnimated bool `json:"is_animated"`
+
+	// IsVideo is True, if the sticker is video sticker.
+	IsVideo bool `json:"is_video"`
+
+	// Thumb is a sticker thumbnail in the .WEBP or .JPG format.
+	//
+	// Optional.
+	Thumb *PhotoSize `json:"thumb,omitempty"`
+
+	// Emoji associated with the sticker.
+	//
+	// Optional.
+	Emoji string `json:"emoji,omitempty"`
+
+	// SetName is a name of the sticker set to which the sticker belongs.
+	//
+	// Optional.
+	SetName string `json:"set_name,omitempty"`
+
+	// MaskPosition is the position where the mask should be placed.
+	// For mask stickers.
+	//
+	// Optional.
+	MaskPosition *MaskPosition `json:"mask_position,omitempty"`
+
+	// FileSize in bytes.
+	//
+	// Optional.
+	FileSize int `json:"file_size,omitempty"`
+}
