0adbb29a7bfb28b9830c5f6d1c9d25e65d0a7847
Vladislav Polyakov <polrkov.tm@gmail.com>
2022-04-04 12:17:30 +0300

    feat: add proximity_alert_triggered

---
+++ bot/api/proximity_alert_triggered.go
@@ -0,0 +1,16 @@
+// Bot API 5.7
+
+package api
+
+// ProximityAlertTriggered is the content of a service message,
+// sent whenever a user in the chat triggers a proximity alert set by another user.
+type ProximityAlertTriggered struct {
+	// Traveler is the User that triggered the alert.
+	Traveler *User `json:"traveler"`
+
+	// Watcher is the User that set the alert.
+	Watcher *User `json:"watcher"`
+
+	// Distance between the users.
+	Distance int `json:"distance"`
+}
